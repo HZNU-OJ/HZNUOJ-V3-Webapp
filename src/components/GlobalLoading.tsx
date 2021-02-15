ddb441addf497617df16bb04129ecb5afb1ea294
Dup4 <lyuzhi.pan@gmail.com>
2021-02-16 01:40:55 +0800

    feat: add monaco editor

---
+++ src/components/GlobalLoading.tsx
@@ -0,0 +1,16 @@
+import Loading from '@/components/Loading';
+
+export default () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Loading />
+    </div>
+  );
+};
