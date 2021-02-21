6fa263a2760bcaead2572614f958c3ae0781335c
Dup4 <lyuzhi.pan@gmail.com>
2021-02-21 20:45:11 +0800

    fix: table filter

--- src/utils/hooks/useTableFilter.tsx
+++ src/utils/hooks/useTableFilter.tsx
@@ -1,15 +1,49 @@
-import React, { useState, useRef } from "react";
-import { Input, Space, Button } from "antd";
+import React, { useState, useEffect, useRef } from "react";
+import { Input, Space, Button, TreeSelect } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 import Highlighter from "react-highlight-words";
 
-export function useTableSearch(dataIndex: string, dataName?: string) {
+import { FilterOutlined } from "@ant-design/icons";
+
+const SHOW_PARENT = TreeSelect.SHOW_PARENT;
+
+export function useTableFilter(dataIndex: string, dataName?: string) {
   dataName = dataName ?? dataIndex;
 
-  const [searchText, setSearchText] = useState("");
+  const [selectedKeys, setSelectedKeys] = useState("");
   const [searchedColumn, setSearchedColumn] = useState("");
 
-  const searchInput = useRef(null);
+  // treeSelect Component => Use treeData to generate a tree structure from JSON data.
+  const itemSelection = (
+    treeData: any,
+    dataIndex: any,
+    selectedKeys: any,
+    setSelectedKeys: any,
+  ) => {
+    // Check the configuration here: https://ant.design/components/tree-select-cn/
+    const tProps = {
+      treeData,
+      value: selectedKeys,
+      defaultValue: [],
+      placeholder: `Select ${dataIndex}`,
+      autoClearSearchValue: false,
+      treeCheckable: true,
+      maxTagCount: 0,
+      treeNodeFilterProp: "title",
+      treeDefaultExpandAll: true,
+      showCheckedStrategy: SHOW_PARENT,
+      getPopupContainer: (triggerNode) => triggerNode.parentNode,
+      size: "small",
+      className: "tree-select",
+      dropdownMatchSelectWidth: 217,
+      dropdownClassName: "common-treeSelect-dropdown",
+      onChange: (value: any) => {
+        setSelectedKeys(value);
+      },
+    };
+
+    return <TreeSelect {...tProps} />;
+  };
 
   const getColumnSearchProps = (dataIndex: string, dataName: string) => ({
     filterDropdown: ({
@@ -63,7 +97,7 @@ export function useTableSearch(dataIndex: string, dataName?: string) {
       searchedColumn === dataIndex ? (
         <Highlighter
           highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
-          searchWords={[searchText]}
+          searchWords={[selectedKeys]}
           autoEscape
           textToHighlight={text.toString()}
         />
@@ -74,13 +108,13 @@ export function useTableSearch(dataIndex: string, dataName?: string) {
 
   const handleSearch = (selectedKeys: any, confirm: any, dataIndex: string) => {
     confirm();
-    setSearchText(selectedKeys[0]);
+    setSelectedKeys(selectedKeys[0]);
     setSearchedColumn(dataIndex);
   };
 
   const handleReset = (clearFilters: any) => {
     clearFilters();
-    setSearchText("");
+    setSelectedKeys("");
   };
 
   return getColumnSearchProps(dataIndex, dataName);
