66fd0abca62ac4f5d6d7a95d7c9c7aca377d8783
Dup4 <lyuzhi.pan@gmail.com>
2021-02-21 14:31:07 +0800

    feat: error page

--- config/routes.ts
+++ config/routes.ts
@@ -2,33 +2,56 @@ export default [
   {
     path: "/",
     component: "@/pages/home/HomePage",
+    exact: true,
   },
   {
     path: "/login",
     component: "@/pages/auth/login/LoginPage",
+    exact: true,
   },
   {
     path: "/register",
     component: "@/pages/auth/register/RegisterPage",
+    exact: true,
   },
   {
     path: "/forgot-password",
     component: "@/pages/auth/forgot-password/ForgotPasswordPage",
+    exact: true,
   },
   {
     path: "/contests",
     component: "@/pages/contest/contests/ContestsPage",
+    exact: true,
   },
   {
     path: "/problemset",
     component: "@/pages/problem/problem-set/ProblemSetPage",
+    exact: true,
   },
   {
     path: "/submissions",
     component: "@/pages/submission/submissions/SubmissionsPage",
+    exact: true,
   },
   {
     path: "/users",
     component: "@/pages/user/users/UsersPage",
+    exact: true,
+  },
+  {
+    path: "/403",
+    component: "@/pages/403",
+    exact: true,
+  },
+  {
+    path: "/404",
+    component: "@/pages/404",
+    exact: true,
+  },
+  {
+    path: "/500",
+    component: "@/pages/500",
+    exact: true,
   },
 ];
