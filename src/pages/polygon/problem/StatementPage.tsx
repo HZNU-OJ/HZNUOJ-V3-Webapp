070c87c67c56a164f411c5f9be6e560c9988e208
Dup4 <lyuzhi.pan@gmail.com>
2021-03-16 02:15:42 +0800

    feat: polygon problem statement

--- src/pages/polygon/problem/StatementPage.tsx
+++ src/pages/polygon/problem/StatementPage.tsx
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import ProblemLayout from "./components/ProblemLayout";
+import LazyMarkDownEditor from "@/components/Editor/LazyMarkDownEditor";
+import { useScreenWidthWithin } from "@/utils/hooks";
+import { Button } from "antd";
 
 const StatementPage: React.FC<{}> = (props) => {
+  const [description, setDescription] = useState("");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [note, setNote] = useState("");
+
+  const isMobile = useScreenWidthWithin(0, 577);
+
+  const mobileW = "220";
+  const DestopW = "360";
+
   return (
     <>
-      <ProblemLayout current={"statement"}></ProblemLayout>
+      <ProblemLayout current={"statement"}>
+        <p>
+          <strong>Description</strong>
+        </p>
+        <LazyMarkDownEditor
+          height={isMobile ? mobileW : DestopW}
+          language={"markdown"}
+          value={description}
+          onChange={(value) => setDescription(value)}
+        />
+        <p>
+          <strong>Input</strong>
+        </p>
+        <LazyMarkDownEditor
+          height={isMobile ? mobileW : DestopW}
+          language={"markdown"}
+          value={input}
+          onChange={(value) => setInput(value)}
+        />
+        <p>
+          <strong>Output</strong>
+        </p>
+        <LazyMarkDownEditor
+          height={isMobile ? mobileW : DestopW}
+          language={"markdown"}
+          value={output}
+          onChange={(value) => setOutput(value)}
+        />
+        <p>
+          <strong>Note</strong>
+        </p>
+        <LazyMarkDownEditor
+          height={isMobile ? mobileW : DestopW}
+          language={"markdown"}
+          value={note}
+          onChange={(value) => setNote(value)}
+        />
+        <Button type={"primary"}>Submit</Button>
+      </ProblemLayout>
     </>
   );
 };
