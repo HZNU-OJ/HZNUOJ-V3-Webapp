5fd8c4345df055da58970c1dbd147ad8dd1811f4
Dup4 <lyuzhi.pan@gmail.com>
2021-03-23 03:18:46 +0800

    feat: homepage Announcement

--- src/pages/home/components/Announcement.tsx
+++ src/pages/home/components/Announcement.tsx
@@ -1,6 +1,9 @@
-import { Tooltip } from "antd";
+import { Tooltip, Skeleton } from "antd";
 import style from "./Announcement.module.less";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { formatDateTime } from "@/utils/formatDateTime";
+
+import api from "@/api";
 
 function getData() {
   const announcemnets = [
@@ -39,20 +42,71 @@ function getData() {
 }
 
 const Announcement: React.FC<{}> = (props) => {
+  const [fetchLoading, setFetchLoading] = useState(true);
+  const [tbody, setTbody] = useState(null);
+
+  async function fetchData() {
+    const { requestError, response } = await api.discussion.queryDiscussions({
+      locale: "en_US",
+      publisherId: 0,
+      nonpublic: false,
+      titleOnly: false,
+      skipCount: 0,
+      takeCount: 0,
+    });
+
+    if (requestError) {
+    } else {
+      let html = [];
+      response.discussions.forEach((item, index: number) => {
+        const date = item.meta.editTime ?? item.meta.publishTime;
+        html.push(
+          <tr key={["announcemnet", index].join("-")}>
+            <td style={{ textAlign: "left" }} className={"h-ellipsis"}>
+              <a href={`/discussion/${item.meta.id}`}>
+                <Tooltip placement="top" title={item.meta.title}>
+                  <span>{item.meta.title}</span>
+                </Tooltip>
+              </a>
+            </td>
+            <td style={{ textAlign: "right", fontFamily: "Georgia" }}>
+              <Tooltip placement="top" title={formatDateTime(date)[1]}>
+                <span>{formatDateTime(date)[0]}</span>
+              </Tooltip>
+            </td>
+          </tr>,
+        );
+      });
+      setTbody(html);
+      setFetchLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="am-panel am-panel-primary" style={{ fontSize: 15 }}>
       <div className="am-panel-hd" style={{ padding: "2px 5px", fontSize: 16 }}>
         Announcement
       </div>
-      <table className="am-table">
-        <thead>
-          <tr>
-            <th className={style.tableTitle}>Title</th>
-            <th className={style.tableDate}>Date</th>
-          </tr>
-        </thead>
-        <tbody>{getData()}</tbody>
-      </table>
+      <Skeleton
+        active
+        title={true}
+        paragraph={{ rows: 4 }}
+        loading={fetchLoading}
+      >
+        <table className="am-table">
+          <thead>
+            <tr>
+              <th className={style.tableTitle}>Title</th>
+              <th className={style.tableDate}>Date</th>
+            </tr>
+          </thead>
+          <tbody>{tbody}</tbody>
+        </table>
+      </Skeleton>
     </div>
   );
 };
