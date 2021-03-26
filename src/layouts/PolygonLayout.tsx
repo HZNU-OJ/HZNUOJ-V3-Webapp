880c5ed1db0eeb3c90f09f8f451b6a55d87ddbf5
Dup4 <lyuzhi.pan@gmail.com>
2021-03-26 10:17:21 +0800

    fix: polygon layout username

--- src/layouts/PolygonLayout.tsx
+++ src/layouts/PolygonLayout.tsx
@@ -23,7 +23,7 @@ function userItemListRender(itemList: menuItem[][], username: string): string {
   html += `<li class="am-dropdown" data-am-dropdown>`;
   html += `
   <a class='am-dropdown-toggle' data-am-dropdown-toggle href='javascript:;'>
-  <span class='am-icon-user'></span>&nbsp;Dup4&nbsp;<span class='am-icon-caret-down'></span>
+  <span class='am-icon-user'></span>&nbsp;${username}&nbsp;<span class='am-icon-caret-down'></span>
   </a>`;
   html += `<ul class="am-dropdown-content">`;
   for (let i = 0; i < itemList.length; ++i) {
