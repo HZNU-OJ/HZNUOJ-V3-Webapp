bfff1addee38acfff92d7feec3fcdf96a10d12a7
Dup4 <lyuzhi.pan@gmail.com>
2021-04-03 00:45:47 +0800

    fix: auth

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import api from "@/api";
 import { useAuthToken } from "@/utils/hooks";
+import { message } from "antd";
 
 export async function getInitialState() {
   const { getToken, signOut } = useAuthToken();
@@ -9,9 +10,13 @@ export async function getInitialState() {
     const { requestError, response } = await api.auth.getSessionInfo({
       token: token,
     });
+
+    if (requestError) message.error(requestError);
+
     if (!response.userMeta) {
       signOut();
     }
+
     return response;
   } catch (error) {
     console.log(error);
