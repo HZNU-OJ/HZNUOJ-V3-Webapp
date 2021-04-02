9a1dce4af5cef654f9a2913f658aa3632ef9fe43
Dup4 <lyuzhi.pan@gmail.com>
2021-04-03 01:11:04 +0800

    feat: some icons

--- src/pages/contest/components/StatusBadge.tsx
+++ src/pages/contest/components/StatusBadge.tsx
@@ -1,4 +1,3 @@
-import { Badge } from "antd";
 import { ContestStatusColor } from "@/interface/Contest";
 
 import style from "./StatusBadge.module.less";
@@ -9,11 +8,5 @@ export const StatusBadge = (status: string) => {
       <div className={[style.label, style[status]].join(" ")}></div>
       <b style={{ color: ContestStatusColor[status] }}>{status}</b>
     </>
-    // <div style={{ fontSize: 14 }}>
-    //   <Badge style={{ fontSize: 14}} color={ContestStatusColor[status]} />
-    //   <b style={{ marginLeft: -5, color: ContestStatusColor[status] }}>
-    //     {status}
-    //   </b>
-    // </div>
   );
 };
