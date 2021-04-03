f34695badf053f617b6f94ea367260a4a4238cfc
Dup4 <lyuzhi.pan@gmail.com>
2021-04-03 08:46:55 +0800

    feat: icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,8 @@
 export * from "./Github";
 export * from "./QQ";
 export * from "./Clock";
+
+export * from "./Girl";
+export * from "./Like";
+export * from "./RightArrow";
+export * from "./Star";
