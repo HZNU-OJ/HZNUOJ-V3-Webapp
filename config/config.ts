10fbc3bbeed8142f276432ba695d26dfc96cf9e4
Dup4 <lyuzhi.pan@gmail.com>
2021-04-14 20:06:07 +0800

    fix: recover page test

--- config/config.ts
+++ config/config.ts
@@ -9,54 +9,54 @@ import { EditorLanguage } from "monaco-editor-webpack-plugin/out/languages";
 import MonacoWebpackPlugin from "monaco-editor-webpack-plugin";
 
 export default defineConfig({
-  // polyfill: {
-  //   imports: ["core-js/stable"],
-  // },
-  // dynamicImport: {
-  //   loading: "@/components/GlobalLoading",
-  // },
-  // chunks: ["basic", "vendors.umi", "umi"],
-  // chainWebpack: (config, { webpack }) => {
-  //   config.merge({
-  //     optimization: {
-  //       splitChunks: {
-  //         chunks: "all",
-  //         minSize: 30000,
-  //         maxSize: 0,
-  //         name: true,
-  //         minChunks: 1,
-  //         maxAsyncRequests: 10,
-  //         maxInitialRequests: 5,
-  //         automaticNameDelimiter: ".",
-  //         cacheGroups: {
-  //           basic: {
-  //             name: "basic",
-  //             test({ resource }) {
-  //               return /(@antd|antd|@ant-design|react)/.test(resource);
-  //             },
-  //             priority: 100,
-  //           },
-  //           monacoEditor: {
-  //             name: true,
-  //             test({ resource }) {
-  //               return /monaco/.test(resource);
-  //             },
-  //             priority: 99,
-  //           },
-  //         },
-  //       },
-  //     },
-  //   });
-  //   //更多配置 https://github.com/Microsoft/monaco-editor-webpack-plugin#options
-  //   config.plugin("monaco-editor-webpack-plugin").use(MonacoWebpackPlugin, [
-  //     // 按需配置
-  //     {
-  //       languages: customConfig.monacoEditorSupportedLanguages as EditorLanguage[],
-  //       publicPath: customConfig.publicPath,
-  //     },
-  //   ]);
-  //   return config;
-  // },
+  polyfill: {
+    imports: ["core-js/stable"],
+  },
+  dynamicImport: {
+    loading: "@/components/GlobalLoading",
+  },
+  chunks: ["basic", "vendors.umi", "umi"],
+  chainWebpack: (config, { webpack }) => {
+    config.merge({
+      optimization: {
+        splitChunks: {
+          chunks: "all",
+          minSize: 30000,
+          maxSize: 0,
+          name: true,
+          minChunks: 1,
+          maxAsyncRequests: 10,
+          maxInitialRequests: 5,
+          automaticNameDelimiter: ".",
+          cacheGroups: {
+            basic: {
+              name: "basic",
+              test({ resource }) {
+                return /(@antd|antd|@ant-design|react)/.test(resource);
+              },
+              priority: 100,
+            },
+            monacoEditor: {
+              name: true,
+              test({ resource }) {
+                return /monaco/.test(resource);
+              },
+              priority: 99,
+            },
+          },
+        },
+      },
+    });
+    //更多配置 https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+    config.plugin("monaco-editor-webpack-plugin").use(MonacoWebpackPlugin, [
+      // 按需配置
+      {
+        languages: customConfig.monacoEditorSupportedLanguages as EditorLanguage[],
+        publicPath: customConfig.publicPath,
+      },
+    ]);
+    return config;
+  },
   extraBabelPlugins: [
     [
       "prismjs",
