49f3dfe2bcaf13c8eca345a2478253699135a776
Dup4 <lyuzhi.pan@gmail.com>
2022-05-27 11:17:51 +0800

    chore: upgrade deps

--- scripts/get_public_assets.sh
+++ scripts/get_public_assets.sh
@@ -9,20 +9,16 @@ cd "${TOP_DIR}"/../public || exit 1
 AMAZEUI="amazeui"
 JQUERY="jquery"
 
-if [[ -d "${AMAZEUI}" ]]; then
-    rm -rf "${AMAZEUI}"
+if [[ ! -d "${AMAZEUI}" ]]; then
+    git clone -b release https://github.com/Dup4/amazeui.git --depth=1
+    rm -rf amazeui/.git
 fi
 
-if [[ -d "${JQUERY}" ]]; then
-    rm -rf "${JQUERY}"
+if [[ ! -d "${JQUERY}" ]]; then
+    mkdir "${JQUERY}"
+
+    cd "${JQUERY}" || exit 1
+
+    wget https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js
+    wget https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.map
 fi
-
-git clone -b release https://github.com/Dup4/amazeui.git --depth=1
-rm -rf amazeui/.git
-
-mkdir "${JQUERY}"
-
-cd "${JQUERY}" || exit 1
-
-wget https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js
-wget https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.map
